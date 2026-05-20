30ab96395552282620fe53ede0f9aa28594c754e
naymmu <medvanio4@gmail.com>
2026-05-20 20:14:20 +0100

    Ingorar teste

---
+++ src/config.example.js
@@ -0,0 +1,6 @@
+export const EMAIL = process.env.EMAIL ?? "teste@gmail.com";
+export const PASSWORD = process.env.PASSWORD ?? "teste1234";
+export const ACCOUNTS_URL =
+  process.env.ACCOUNTS_URL ??
+  "https://app.nbanks.net/treasury/wallets/accounts";
+export const SIGNIN_URL = "https://app.nbanks.net/auth/signin";
